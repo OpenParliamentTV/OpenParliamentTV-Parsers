6111f046db9b26d5e34af713b7271f716b9f11bf
Olivier Aubert <contact@olivieraubert.net>
2021-06-25 14:44:00 +0200

    Cache URL info into index.txt

--- parliaments/DE/scraper/fetch_proceedings.py
+++ parliaments/DE/scraper/fetch_proceedings.py
@@ -25,6 +25,7 @@ def download_plenary_protocols(destination_dir: str, fullscan: bool = False):
     if not dest.is_dir():
         dest.mkdir(parents=True)
     http = urllib3.PoolManager()
+    index_file = open(dest / "index.txt", 'wt+')
     offset = 0
     while True:
         logger.debug(f"Fetching RSS with offset {offset}")
@@ -34,7 +35,8 @@ def download_plenary_protocols(destination_dir: str, fullscan: bool = False):
         for link in parsed.getiterator(tag="a"):
             link_href = link.attrib["href"]
             link_count += 1
-            filename = dest / os.path.basename(link_href)
+            basename = os.path.basename(link_href)
+            filename = dest / basename
             if filename.exists():
                 # Existing file.
                 if not fullscan:
@@ -45,10 +47,13 @@ def download_plenary_protocols(destination_dir: str, fullscan: bool = False):
                 file_url = f"{SERVER_ROOT}{link_href}"
                 logger.info(f"downloading URL {file_url}")
                 urllib.request.urlretrieve(file_url, filename)
+                # Add URL reference to index.txt
+                index_file.write(f"{basename} {file_url}\n")
         if link_count == 0:
             # Empty file, end of data
             break
         offset += link_count
+    index_file.close()
 
 if __name__ == "__main__":
 
