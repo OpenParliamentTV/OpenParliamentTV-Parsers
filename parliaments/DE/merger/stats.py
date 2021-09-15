d94982bfebfb5727acbe2119606e0a2e5cb0c387
Olivier Aubert <contact@olivieraubert.net>
2021-09-16 00:05:04 +0200

    Compute global stats

--- parliaments/DE/merger/stats.py
+++ parliaments/DE/merger/stats.py
@@ -3,11 +3,10 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import json
 from pathlib import Path
-import sys
+import statistics
 
-from merge_session import unmatched_count, diff_files
+from merge_session import unmatched_count
 
 BASE_DIR = Path(__file__).resolve().parent.parent / 'data'
 DATA_DIR = BASE_DIR / 'merged'
@@ -15,18 +14,20 @@ PROCEEDINGS_DIR = BASE_DIR / 'proceedings'
 MEDIA_DIR = BASE_DIR / 'media'
 
 if __name__ == '__main__':
-    pcounts = []
-    mcounts = []
-    print("""Session\tProc size\tMedia size\tUnmatched proceedings\tUnmatched media""")
+    print("""Session\tProc size\tMedia size\tUnmatched proceedings\tUnmatched media\tUnmatched media relative""")
+    data = []
     for m in sorted(MEDIA_DIR.glob('1*.json')):
         # Find basename
         session = str(m.name)[:5]
         p = PROCEEDINGS_DIR / f'{session}-data.json'
         if p.exists():
             count = unmatched_count(p, m)
-            print(f"""{session}\t{count['proceedings']}\t{count['media']}\t{count['unmatched_proceedings']}\t{count['unmatched_media']}""")
-            pcounts.append(count['proceedings'])
-            mcounts.append(count['media'])
+            data.append(count)
+            print(f"""{session}\t{count['proceedings']}\t{count['media']}\t{count['unmatched_proceedings']}\t{count['unmatched_media']}\t{count['unmatched_media'] / count['media']}""")
         else:
             print(f"""{session}\tUNMATCHED""")
     # Global stats
+    size = len(data)
+    ratio_list = [ c['unmatched_media'] / c['media'] for c in data ]
+    print(f"""Average unmatched media ratio: { statistics.mean(ratio_list) }
+Median unmatched media ratio: { statistics.median(ratio_list) }""")
