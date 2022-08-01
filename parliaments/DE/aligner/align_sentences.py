223c0316db2ec010141868c0413f1ed28b616363
Olivier Aubert <contact@olivieraubert.net>
2022-08-01 16:52:22 +0200

    Add parameters for cache dir and language

--- parliaments/DE/aligner/align_sentences.py
+++ parliaments/DE/aligner/align_sentences.py
@@ -6,6 +6,7 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import argparse
 import json
 import os
 from pathlib import Path
@@ -18,10 +19,7 @@ from aeneas.task import Task
 
 # We want to check that we have 1GB minimum available cache size
 MIN_CACHE_SPACE = 1024 * 1024 * 1024
-
-AUDIOCACHEDIR = Path('/tmp/cached')
-if not AUDIOCACHEDIR.is_dir():
-    AUDIOCACHEDIR.mkdir()
+DEFAULT_CACHEDIR = '/tmp/cache'
 
 def sentence_iter(speech: dict) -> iter:
     """Iterate over all sentences in a speech, adding a unique identifier.
@@ -33,13 +31,15 @@ def sentence_iter(speech: dict) -> iter:
                 ident = f"s{speechIndex}-{contentIndex}-{bodyIndex}-{sentenceIndex}"
                 yield ident, sentence
 
-def cachedfile(speech: dict, extension: str) -> Path:
+def cachedfile(speech: dict, extension: str, cachedir: Path = None) -> Path:
     """Return a filename with given extension
     """
     filename = f"{speech['session']['number']}{speech['agendaItem']['speechIndex']}.{extension}"
-    return AUDIOCACHEDIR / filename
+    if not cachedir.is_dir():
+        cachedir.mkdir()
+    return cachedir / filename
 
-def audiofile(speech: dict) -> Path:
+def audiofile(speech: dict, cachedir: Path = None) -> Path:
     """Get an audiofile for the given dict.
 
     Either it is already cached (return filename) or download it
@@ -47,10 +47,10 @@ def audiofile(speech: dict) -> Path:
 
     If anything wrong happens, return None
     """
-    audio = cachedfile(speech, 'mp3')
+    audio = cachedfile(speech, 'mp3', cachedir)
     if not audio.exists():
         # Check that we have enough disk space for caching
-        total, used, free = shutil.disk_usage(AUDIOCACHEDIR)
+        total, used, free = shutil.disk_usage(cachedir)
         if free < MIN_CACHE_SPACE:
             logger.error(f"No enough disk space for cache dir: {free / 1024 / 1024 / 1024} GB")
             return None
@@ -69,27 +69,40 @@ def audiofile(speech: dict) -> Path:
     return audio
 
 
-def align_audio(source: list) -> list:
+def align_audio(source: list, language: str, cachedir: Path = None) -> list:
     """Align list of speeches to add timing information to sentences.
 
     The structure is modified in place, and returned.
     """
+    if cachedir is None:
+        cachedir = Path(DEFAULT_CACHEDIR)
+        logger.warning(f"No cache dir specified - using default {cachedir}")
+    else:
+        cachedir = Path(cachedir)
+
     for speech in source:
+        # Do we have proceedings data?
+        text_data = [ "|".join((ident, sentence['text'])) + os.linesep
+                      for ident, sentence in sentence_iter(speech) ]
+        if len(text_data) == 0:
+            logger.warning(f"No text data to align - skipping {speech['session']['number']}{speech['agendaItem']['speechIndex']}")
+            continue
+
         # Download audio file
-        audio = audiofile(speech)
+        audio = audiofile(speech, cachedir)
         if audio is None:
             continue
 
         # Generate parsed text format file with identifier + sentence
-        sentence_file = cachedfile(speech, 'txt')
+        sentence_file = cachedfile(speech, 'txt', cachedir)
         with open(sentence_file, 'wt') as sf:
-            for ident, sentence in sentence_iter(speech):
-                sf.write("|".join( (ident, sentence['text'])) + os.linesep)
+            sf.writelines(text_data)
 
+        logger.warning(f"Aligning {sentence_file} with {audio}")
         # Do the alignment
-        task = Task(config_string="""task_language=deu|task_language=eng|is_text_type=parsed|os_task_file_format=json""")
-        task.audio_file_path_absolute = str(audio)
-        task.text_file_path_absolute = str(sentence_file)
+        task = Task(config_string=f"""task_language={language}|is_text_type=parsed|os_task_file_format=json""")
+        task.audio_file_path_absolute = str(audio.absolute())
+        task.text_file_path_absolute = str(sentence_file.absolute())
         # process Task
         ExecuteTask(task).execute()
 
@@ -111,11 +124,23 @@ def align_audio(source: list) -> list:
     return source
 
 if __name__ == '__main__':
-    merged_source = sys.argv[1]
-    merged_dest = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Time-align speech sentences.")
+    parser.add_argument("source", metavar="source", type=str, nargs='?',
+                        help="Source file (merged format)")
+    parser.add_argument("destination", metavar="destination", type=str, nargs='?',
+                        help="Destination file")
+    parser.add_argument("--lang", type=str, default="deu",
+                        help="Language")
+    parser.add_argument("--cache-dir", type=str, default=None,
+                        help="Cache directory")
 
-    with open(merged_source) as f:
+    args = parser.parse_args()
+    if args.source is None:
+        parser.print_help()
+        sys.exit(1)
+
+    with open(args.source) as f:
         source = json.load(f)
-    output = align_audio(source)
-    with open(merged_dest, 'w') as f:
+    output = align_audio(source, args.lang, args.cache_dir)
+    with (open(args.destination, 'w') if args.destination else sys.stdout) as f:
         json.dump(output, f)
